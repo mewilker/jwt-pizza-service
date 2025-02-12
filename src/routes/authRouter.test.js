82ebee1418da3ff6502691b5526ed39f4f07291e
mewilker <mewilk@byu.edu>
2025-02-12 10:27:41 -0700

    created a testUtils to remove some duplication

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../service');
+const utils = require('../testUtils')
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
@@ -12,13 +13,13 @@ beforeAll(async () => {
   testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
   const registerRes = await request(app).post('/api/auth').send(testUser);
   testUserAuthToken = registerRes.body.token;
-  expectValidJwt(registerRes.body.token);
+  utils.expectValidJwt(registerRes.body.token);
 });
 
 test('login', async () => {
   const loginRes = await request(app).put('/api/auth').send(testUser);
   expect(loginRes.status).toBe(200);
-  expectValidJwt(loginRes.body.token);
+  utils.expectValidJwt(loginRes.body.token);
 
   const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
   expect(loginRes.body.user).toMatchObject(user);
@@ -34,7 +35,7 @@ test('register', async () =>{
   newUser.email = Math.random().toString(36).substring(2,12);
   const response = await request(app).post('/api/auth').send(newUser);
   expect(response.status).toBe(200);
-  expectValidJwt(response.body.token);
+  utils.expectValidJwt(response.body.token);
   
   const { password, ...user } = { ...newUser, roles: [{ role: 'diner' }] };
   expect(response.body.user).toMatchObject(user);
@@ -57,7 +58,3 @@ test('logout bad auth', async () =>{
   const response = await request(app).delete('/api/auth').set("Authorization", `Bearer badboy`).send();
   expect(response.status).toBe(401);
 })
-
-function expectValidJwt(potentialJwt) {
-  expect(potentialJwt).toMatch(/^[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*\.[a-zA-Z0-9\-_]*$/);
-}
