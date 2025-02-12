74458164cbc5af195bdc4fa2286ca75f19974897
mewilker <mewilk@byu.edu>
2025-02-12 13:24:35 -0700

    moreFranchise tests

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -40,7 +40,52 @@ test('createFranchise for TestUser', async () => {
   .post('/api/franchise')
   .set("Authorization", `Bearer ${adminToken}`)
   .send({name:franchiseName, admins:[{email:testUser.email}]});
-  console.log(response.body);
   expect(response.status).toBe(200);
   expect(response.body).toEqual(expect.objectContaining({id: expect.any(Number), name: franchiseName, admins:[{email:testUser.email, id: expect.any(Number), name:'Franchise OwnerToBe'}]}))
+})
+
+test('createFranchise fails when not admin', async () =>{
+  const franchiseName = utils.randomName();
+  const response = await request(app)
+  .post('/api/franchise')
+  .set("Authorization", `Bearer ${testUserAuthToken}`)
+  .send({name:franchiseName, admins:[{email:testUser.email}]});
+  expect(response.status).toBe(403);
+})
+
+test('createFranchise cannot use duplicate names', async() =>{
+  const franchiseName = utils.randomName();
+  const response = await request(app)
+  .post('/api/franchise')
+  .set("Authorization", `Bearer ${adminToken}`)
+  .send({name:franchiseName, admins:[{email:testUser.email}]});
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual(expect.objectContaining({id: expect.any(Number), name: franchiseName, admins:[{email:testUser.email, id: expect.any(Number), name:'Franchise OwnerToBe'}]}))
+  const duppedResponse = await request(app)
+  .post('/api/franchise')
+  .set("Authorization", `Bearer ${adminToken}`)
+  .send({name:franchiseName, admins:[{email:testUser.email}]});
+  expect(duppedResponse.status).toBe(500);
+})
+
+test('deleteFranchise', async() =>{
+  const franchiseName = utils.randomName();
+  const response = await request(app)
+  .post('/api/franchise')
+  .set("Authorization", `Bearer ${adminToken}`)
+  .send({name:franchiseName, admins:[{email:testUser.email}]});
+  expect(response.status).toBe(200);
+  expect(response.body).toEqual(expect.objectContaining({id: expect.any(Number), name: franchiseName, admins:[{email:testUser.email, id: expect.any(Number), name:'Franchise OwnerToBe'}]}))
+  const deleteResponse = await request(app)
+  .delete(`/api/franchise/${response.body.id}`)
+  .set("Authorization", `Bearer ${adminToken}`)
+  .send();
+  expect(deleteResponse.status).toBe(200);
+  const listResponse =  await request(app)
+    .get('/api/franchise')
+    .set("Authorization", `Bearer ${testUserAuthToken}`)
+    .send();
+  expect(listResponse.status).toBe(200);
+  expect(listResponse.body).toBeInstanceOf(Array)
+  expect(listResponse.body).not.toContain(expect.objectContaining({id: response.body.id, name:franchiseName, stores: expect.any(Array)}))
 })
