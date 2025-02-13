760f4b171f315f567e497eddb6649728a1e3562a
mewilker <mewilk@byu.edu>
2025-02-13 11:55:03 -0700

    with way more code than should be necessary... make order now dynamically makes an menu item to order... so it should pass github now.

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -2,8 +2,12 @@ const request = require('supertest');
 const app = require('../service');
 const utils = require('../testUtils');
 
-const plebUser = { name: 'sneaky dinner', email: 'TBD', password: 'a'}
+const plebUser = { name: 'hungry dinner', email: 'TBD', password: 'a'}
 let plebToken;
+let adminUser;
+let adminToken;
+const franchisee = {name: "im sodone", email:'TBD', password: 'a'}
+let franchiseeToken;
 
 if (process.env.VSCODE_INSPECTOR_OPTIONS) {
     jest.setTimeout(60 * 1000 * 5); // 5 minutes
@@ -14,6 +18,14 @@ beforeAll(async () => {
     const response = await request(app).post('/api/auth').send(plebUser);
     plebToken = response.body.token;
     utils.expectValidJwt(response.body.token);
+    franchisee.email = utils.randomName() + '@test.com';
+    const franchiseeRes = await request(app).post('/api/auth').send(franchisee);
+    franchiseeToken = franchiseeRes.body.token;
+    utils.expectValidJwt(franchiseeRes.body.token);
+    adminUser = await utils.createAdminUser();
+    const adminRes = await request(app).put('/api/auth').send(adminUser);
+    adminToken = adminRes.body.token;
+    utils.expectValidJwt(adminRes.body.token);
 });
 
 test('getMenu', async () =>{
@@ -36,12 +48,17 @@ test('make Order unauthenticated', async () =>{
 })
 
 test('makeOrder', async () =>{
+    const menuItem = await createMenuItem();
+    const franchiseInfo = await successfulFranchiseCreate();
+    const storeInfo = await successfulStoreCreate(franchiseInfo, adminToken)
     const response = await request(app)
     .post('/api/order')
     .set("Authorization", `Bearer: ${plebToken}`)
-    .send({franchiseId: 1, storeId:1, items:[{ menuId: 1, description: "Veggie", price: 0.05 }]});
+    .send({franchiseId: franchiseInfo.id, storeId:storeInfo.id, items:[{ menuId: menuItem.id, description: menuItem.description, price: menuItem.price }]});
+    console.log(response.body);
     expect(response.status).toBe(200);
-    expect(response.body).toEqual(expect.objectContaining({order:{franchiseId:1, storeId:1, items: [{menuId:1, description:'Veggie', price: 0.05}], id:expect.any(Number)}}));
+    expect(response.body).toEqual(expect.objectContaining({order:{franchiseId:franchiseInfo.id, storeId:storeInfo.id, items: expect.any(Array), id:expect.any(Number)}}));
+    expect(response.body.order.items).toContainEqual(expect.objectContaining({menuId:menuItem.id, description:menuItem.description, price: menuItem.price}));
 })
 
 test('getOrders', async () =>{
@@ -60,3 +77,67 @@ test('cant add MenuItem as non-admin', async () =>{
     .send();
     expect(response.status).toBe(403);
 })
+
+test('add MenuItem', async() =>{
+    await createMenuItem();
+});
+
+async function createMenuItem () {
+    const itemName = utils.randomName();
+    const itemDescription = utils.randomName();
+    const itemImage = utils.randomName() + ".png";
+    const itemPrice = 0.0001;
+    let itemID;
+    const response = await request(app)
+    .put('/api/order/menu')
+    .set("Authorization", `Bearer: ${adminToken}`)
+    .send({title:itemName, 
+        description: itemDescription, 
+        image:itemImage,
+        price: itemPrice });
+    console.log(response.body);
+    expect(response.status).toBe(200);
+    expect(response.body).toContainEqual(expect.objectContaining({
+        id:expect.any(Number),
+        title:itemName,
+        description:itemDescription,
+        image: itemImage,
+        price: itemPrice
+    }));
+     for (let i=response.body.length-1; i>-1; i--){
+        const menu = response.body[i];
+        if (menu.title === itemName && menu.description === itemDescription){
+            itemID = menu.id;
+            break;
+        }
+    }
+    return {
+        id: itemID,
+        title: itemName,
+        description: itemDescription,
+        image: itemImage,
+        price: itemPrice,
+    }
+}
+
+async function successfulStoreCreate(franchiseInfo, token){
+    const storeName = utils.randomName();
+    const response = await request(app)
+    .post(`/api/franchise/${franchiseInfo.id}/store`)
+    .set("Authorization", `Bearer ${token}`)
+    .send({name:storeName});
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(expect.objectContaining({id: expect.any(Number), name: storeName, franchiseId:franchiseInfo.id}))
+    return {id: response.body.id, name: storeName}
+  }
+  
+  async function successfulFranchiseCreate(){
+    const franchiseName = utils.randomName();
+    const response = await request(app)
+    .post('/api/franchise')
+    .set("Authorization", `Bearer ${adminToken}`)
+    .send({name:franchiseName, admins:[{email:franchisee.email}]});
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(expect.objectContaining({id: expect.any(Number), name: franchiseName, admins:[{email:franchisee.email, id: expect.any(Number), name:franchisee.name}]}))
+    return {id:response.body.id, name:franchiseName};
+  }
