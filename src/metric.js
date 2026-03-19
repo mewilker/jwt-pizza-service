02ede443796024e985c7a05ac45b05177a6e29b7
mewilker <mewilk@byu.edu>
2026-03-18 21:37:30 -0600

    deliverable 8: add grafana helper methods

---
+++ src/metric.js
@@ -0,0 +1,73 @@
+const config = require('./config');
+
+//memory store for metrics
+
+
+setInterval(() => {
+  const metrics = [];
+  //metrics.push(createMetric());
+
+  sendMetricToGrafana(metrics);
+}, 10000);
+
+function createMetric(metricName, metricValue, metricUnit, metricType, valueType, attributes) {
+  attributes = { ...attributes, source: config.metrics.source };
+
+  const metric = {
+    name: metricName,
+    unit: metricUnit,
+    [metricType]: {
+      dataPoints: [
+        {
+          [valueType]: metricValue,
+          timeUnixNano: Date.now() * 1000000,
+          attributes: [],
+        },
+      ],
+    },
+  };
+
+  Object.keys(attributes).forEach((key) => {
+    metric[metricType].dataPoints[0].attributes.push({
+      key: key,
+      value: { stringValue: attributes[key] },
+    });
+  });
+
+  if (metricType === 'sum') {
+    metric[metricType].aggregationTemporality = 'AGGREGATION_TEMPORALITY_CUMULATIVE';
+    metric[metricType].isMonotonic = true;
+  }
+
+  return metric;
+}
+
+function sendMetricToGrafana(metrics) {
+  const body = {
+    resourceMetrics: [
+      {
+        scopeMetrics: [
+          {
+            metrics,
+          },
+        ],
+      },
+    ],
+  };
+
+  fetch(`${config.metrics.endpointUrl}`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: { Authorization: `Bearer ${config.metrics.accountId}:${config.metrics.apiKey}`, 'Content-Type': 'application/json' },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP status: ${response.status}`);
+      }
+    })
+    .catch((error) => {
+      console.error('Error pushing metrics:', error);
+    });
+}
+
+module.exports = {};
