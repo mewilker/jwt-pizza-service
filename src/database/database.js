843d2180f823dbba29a4a5573c4ede334d51589c
mewilker <mewilk@byu.edu>
2026-03-24 22:50:37 -0600

    deliverable 9: add logging to database

--- src/database/database.js
+++ src/database/database.js
@@ -4,6 +4,7 @@ const config = require('../config.js');
 const { StatusCodeError } = require('../endpointHelper.js');
 const { Role } = require('../model/model.js');
 const dbModel = require('./dbModel.js');
+const logger = require('../logger.js');
 class DB {
   constructor() {
     this.initialized = this.initializeDatabase();
@@ -274,6 +275,7 @@ class DB {
 
   async query(connection, sql, params) {
     const [results] = await connection.execute(sql, params);
+    logger.log('info', 'database', { sql, params: JSON.stringify(params) });
     return results;
   }
 
